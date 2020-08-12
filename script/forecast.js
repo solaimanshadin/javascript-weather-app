50251416db8411b6e6559f7b5cfc7274d9da224f
solaimanshadin <solaimanshadin@gmail.com>
2020-08-11 19:48:14 -0700

    Done

--- script/forecast.js
+++ script/forecast.js
@@ -1,7 +1,7 @@
 const key = 'nvMZdEkTRPeQtZ56CmUCGXDa7jEY3vjO';
 // get Weather Information
 const getWeather = async(id) => {
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
     const query = `${id}?apikey=${key}`;
 
     const response = await fetch(base + query)
@@ -11,7 +11,7 @@ const getWeather = async(id) => {
 
 //get City Infrmation
 const getCity = async(city) => {
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
     const response = await fetch(base + query);
     const data = await response.json();
